fa37d46739660836b89fcae0d9c2487660fc798b
pclundaahl <pc.lundaahl@gmail.com>
2018-11-10 15:09:44 -0800

    Set up auth folder, with router and static folder

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const auth = require('./auth/router');
 
-const PORT = process.env.PORT || 80;
-
+const PORT = process.env.PORT || 8080;
 let app = express();
-
+app.use('/auth', auth);
 
 app.listen(PORT, () => console.log(`BadgeBook Server listening on port ${PORT}!`));
